94dbf397569d513669ed4922d836853d6348e3e6
AntonTimberg <antonoffice2015@gmail.com>
2023-10-06 14:02:55 +0300

    - add ItemPropertyChangeEvent & CollectionChangeEvent

--- src/main/java/com/company/testing/screen/user/UserEdit.java
+++ src/main/java/com/company/testing/screen/user/UserEdit.java
@@ -8,17 +8,23 @@ import io.jmix.core.DataManager;
 import io.jmix.core.EntityStates;
 import io.jmix.core.security.event.SingleUserPasswordChangeEvent;
 import io.jmix.ui.Notifications;
+import io.jmix.ui.UiComponents;
 import io.jmix.ui.component.Button;
+import io.jmix.ui.component.CheckBox;
 import io.jmix.ui.component.ComboBox;
+import io.jmix.ui.component.Component;
 import io.jmix.ui.component.PasswordField;
 import io.jmix.ui.component.TextField;
+import io.jmix.ui.model.CollectionContainer;
 import io.jmix.ui.model.CollectionPropertyContainer;
 import io.jmix.ui.model.DataContext;
+import io.jmix.ui.model.InstanceContainer;
 import io.jmix.ui.navigation.Route;
 import io.jmix.ui.screen.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
@@ -29,6 +35,9 @@ import java.util.TimeZone;
 @EditedEntityContainer("userDc")
 @Route(value = "users/edit", parentPrefix = "users")
 public class UserEdit extends StandardEditor<User> {
+    @Autowired
+    private UiComponents uiComponents;
+
     @Autowired
     private DataManager dataManager;
 
@@ -132,6 +141,46 @@ public class UserEdit extends StandardEditor<User> {
                 userStep.setSortValue(step.getSortValue());
                 stepsDc.getMutableItems().add(userStep);
             }
+        }
+    }
+
+    @Install(to = "stepsTable.completed", subject = "columnGenerator")
+    private Component stepsTableCompletedColumnGenerator(final UserStep userStep) {
+        CheckBox checkBox = uiComponents.create(CheckBox.class);
+        checkBox.setValue(userStep.getCompletedDate() != null);
+        checkBox.addValueChangeListener(e -> {
+            if (userStep.getCompletedDate() == null) {
+                userStep.setCompletedDate(LocalDate.now());
+            } else {
+                userStep.setCompletedDate(null);
+            }
+        });
+        return checkBox;
+    }
+
+    @Subscribe(id = "stepsDc", target = Target.DATA_CONTAINER)
+    public void onStepsDcItemPropertyChange(final InstanceContainer.ItemPropertyChangeEvent<UserStep> event) {
+        updateOnboardingStatus();
+    }
+
+    @Subscribe(id = "stepsDc", target = Target.DATA_CONTAINER)
+    public void onStepsDcCollectionChange(final CollectionContainer.CollectionChangeEvent<UserStep> event) {
+        updateOnboardingStatus();
+    }
+
+    private void updateOnboardingStatus() {
+        User user = getEditedEntity();
+
+        long completedCount = user.getSteps() == null ? 0 :
+                user.getSteps().stream()
+                        .filter(us -> us.getCompletedDate() != null)
+                        .count();
+        if (completedCount == 0) {
+            user.setOnboardingStatus(OnboardingStatus.NOT_STARTED);
+        } else if (completedCount == user.getSteps().size()) {
+            user.setOnboardingStatus(OnboardingStatus.COMPLETED);
+        } else {
+            user.setOnboardingStatus(OnboardingStatus.IN_PROGRESS);
         }
     }
 }
