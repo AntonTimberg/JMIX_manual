8cd05ebcda9da7a4c1385afa86ff1aee1dfc9e41
AntonTimberg <antonoffice2015@gmail.com>
2023-10-09 19:38:33 +0300

    - add onboarding step screen

---
+++ src/main/java/com/company/testing/screen/myonboarding/MyOnboardingScreen.java
@@ -0,0 +1,28 @@
+package com.company.testing.screen.myonboarding;
+
+import com.company.testing.entity.User;
+import com.company.testing.entity.UserStep;
+import io.jmix.core.security.CurrentAuthentication;
+import io.jmix.ui.model.CollectionLoader;
+import io.jmix.ui.screen.Screen;
+import io.jmix.ui.screen.Subscribe;
+import io.jmix.ui.screen.UiController;
+import io.jmix.ui.screen.UiDescriptor;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@UiController("MyOnboardingScreen")
+@UiDescriptor("my-onboarding-screen.xml")
+public class MyOnboardingScreen extends Screen {
+    @Autowired
+    private CurrentAuthentication currentAuthentication;
+
+    @Autowired
+    private CollectionLoader<UserStep> userStepsDl;
+
+    @Subscribe
+    public void onBeforeShow(BeforeShowEvent event) {
+        User user = (User) currentAuthentication.getUser();
+        userStepsDl.setParameter("user", user);
+        userStepsDl.load();
+    }
+}
